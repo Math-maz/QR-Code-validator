d67163c5f1a9b14f2e274ab025b89edfaec85f0a
Math-maz <matheuspereiraaraujo@MacBook-Pro-de-Regina.local>
2019-10-02 10:01:31 -0300

    Add new lib for input reading

--- readInput.js
+++ readInput.js
@@ -1,17 +1,18 @@
-const readline = require('readline');
-function readInput(){
-    return new Promise((resolve, reject) => {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-          });
-          
-          rl.question('qr_code: ', (qrcode) => {
-            console.log("Validando Ticket...");
-            resolve(qrcode);
-            rl.close();
-          });
-    })
+const readline = require("readline");
+const readlineSync = require("readline-sync");
+function readInput() {
+  return readlineSync.question("qr_code: ");
+  // return new Promise((resolve, reject) => {
+  //     const rl = readline.createInterface({
+  //         input: process.stdin,
+  //         output: process.stdout
+  //       });
 
+  //       rl.question('qr_code: ', (qrcode) => {
+  //         console.log("Validando Ticket...");
+  //         resolve(qrcode);
+  //         rl.close();
+  //       });
+  // })
 }
 module.exports = readInput;
--- validar.js
+++ validar.js
@@ -1,23 +1,26 @@
 const readInput = require("./readInput.js");
 const axios = require("axios");
-function validar(){
-    Promise.resolve(readInput())
-    .then(qrcode => {
-        return axios.post('https://ticket-project.herokuapp.com/api/tickets', {
-            qr_code: qrcode
-        });
+function validar() {
+  const qrCode = readInput();
+  axios
+    .post("https://ticket-project.herokuapp.com/api/tickets", {
+      qr_code: qrCode
     })
-    .then((res) => {
-        if(res.status && res.status == 200){
-            console.log(`HTTP code ${res.status}, o Ticket foi validado com Sucesso!`);
-        } else {
-            console.log(res.status);
-        }
-        validar();
+    .then(res => {
+      if (res.status && res.status == 200) {
+        console.log(
+          `HTTP code ${res.status}, o Ticket foi validado com Sucesso!`
+        );
+      } else {
+        console.log(res.status);
+      }
+      validar();
     })
-    .catch((error) => {
-        console.log(`HTTP code ${error.response.data.error.status}, Ticket já foi validado ou nao foi encontrado`);
-        validar();
-    })  
+    .catch(error => {
+      console.log(
+        `HTTP code ${error.response.data.error.status}, Ticket já foi validado ou nao foi encontrado`
+      );
+      validar();
+    });
 }
 module.exports = validar;
